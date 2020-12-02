475a6fb66cce3d5f92213208122fdd047f796668
cpusillo <cpusillo@gmail.com>
2020-12-01 21:44:04 -0500

    Fixed routing

--- controllers/burgers_controller.js
+++ controllers/burgers_controller.js
@@ -1,5 +1,11 @@
 const express = require("express");
 const burger = require("../models/burger.js")
-const app = express();
 
 // Create the router for the app, and export the router at the end of your file.
+var router = express.Router();
+
+router.get("/", function(req,res){
+    res.sendFile(path.join(__dirname, "public/index.html"));
+});
+
+module.exports = router;
