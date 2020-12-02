34be419bb84c1384934b2d48031b0361e96a1fbe
cpusillo <cpusillo@gmail.com>
2020-12-01 22:22:55 -0500

    Show burgers on index.html

---
+++ public/assets/js/burgers.js
@@ -0,0 +1,32 @@
+$(document).ready (function(){
+
+$(function(){
+    $.ajax("/burgers",{
+    type: "GET",
+    }).then(function(data){
+        let eatBurger = $("#eat-burger");
+        let deleteBurger = $("#delete-burger");
+
+        let burgers = data.burgers;
+        let length = burgers.length;
+
+        let ul_elem_eat = $("#eat-burger")
+        let ul_elem_del = $("#delete-burger")
+
+        for (var i = 0; i < length; i++) {
+            if (burgers[i].devoured === 0){
+            ul_elem_eat.append("<li>"+
+            burgers[i].burger_name+
+            "<button data-burgerid='"+
+            burgers[i].id+"' class='btn btn-danger eat'>Eat!</button></li>")
+        }
+        else {
+            ul_elem_del.append("<li>"+
+            burgers[i].burger_name+
+            "<button data-burgerid='"+
+            burgers[i].id+"' class='btn btn-danger eat'>Delete!</button></li>")
+        }
+          }
+    })
+})
+});
