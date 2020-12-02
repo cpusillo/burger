64fd244b5c5b6f0cc7de4514bbc64fb99a9a016f
cpusillo <cpusillo@gmail.com>
2020-12-01 21:54:11 -0500

    Finished orm.js

--- config/orm.js
+++ config/orm.js
@@ -17,7 +17,7 @@ var orm = {
       cb(result);
     });
   },
-  create: function (table, cols, vals, cb) {
+  insertOne: function (table, cols, vals, cb) {
     var queryString = "INSERT INTO " + table;
 
     queryString += " (";
@@ -37,7 +37,24 @@ var orm = {
       cb(result);
     });
   },
-  delete: function (table, condition, cb) {
+  updateOne: function (table, objColVals, condition, cb) {
+    var queryString = "UPDATE " + table;
+
+    queryString += " SET ";
+    queryString += objToSql(objColVals);
+    queryString += " WHERE ";
+    queryString += condition;
+
+    console.log(queryString);
+    connection.query(queryString, function (err, result) {
+      if (err) {
+        throw err;
+      }
+
+      cb(result);
+    });
+  },
+  deleteOne: function (table, condition, cb) {
     var queryString = "DELETE FROM " + table;
     queryString += " WHERE ";
     queryString += condition;
