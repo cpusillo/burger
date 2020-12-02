c7c85f57530ede468f919d6ac0c018736a54904e
cpusillo <cpusillo@gmail.com>
2020-12-02 14:31:46 -0500

    Added screenshot for documentation

--- server.js
+++ server.js
@@ -1,7 +1,5 @@
 var express = require("express");
 
-var PORT = process.env.PORT || 8080;
-
 var app = express();
 
 // Serve static content for the app from the "public" directory in the application directory.
@@ -16,7 +14,7 @@ var routes = require("./controllers/burgers_controller.js");
 app.use(routes);
 
 // Start our server so that it can begin listening to client requests.
-app.listen(PORT, function() {
+app.listen(process.env.PORT || 8080, function() {
   // Log (server-side) when our server has started
-  console.log(`Server listening on: http://localhost:${PORT}`);
+  console.log(`Server listening`);
 });
