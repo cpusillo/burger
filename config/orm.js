08373dde24fc01b6c97f18edcd9b18bcf3f3095a
cpusillo <cpusillo@gmail.com>
2020-12-01 20:32:24 -0500

    Finished MVC scaffolding

--- config/orm.js
+++ config/orm.js
@@ -1,5 +1,20 @@
 var connection = require("./connection.js");
 
-var orm = {};
+var orm = {
+    /*
+    selectAll()
+    insertOne()
+    updateOne()
+    deleteOne()
+*/
+// SelectAll queries a table for all information.
+selectAll: function(tableInput, colToSearch, valOfCol) {
+    var queryString = "SELECT * FROM ?? WHERE ?? = ?";
+    connection.query(queryString, [tableInput, colToSearch, valOfCol], function(err, result) {
+      if (err) throw err;
+      console.log(result);
+    });
+  },
+};
 
 module.exports = orm;
