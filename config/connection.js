18affa44ed86d16cd3068311787c58aff60b8817
cpusillo <cpusillo@gmail.com>
2020-12-02 13:49:15 -0500

    Took away cleardb

--- config/connection.js
+++ config/connection.js
@@ -1,10 +1,11 @@
 var mysql = require("mysql");
 
 var connection = mysql.createConnection({
-  HOST: "us-cdbr-east-02.cleardb.com",
-  USER: "bb46dc47d7994e",
-  PASSWORD: "8b7f35c5",
-  DATABASE: "heroku_d94fa42ebf84e03"
+  host: "localhost",
+  port: 3306,
+  user: "root",
+  password: "password",
+  database: "burgers_db"
 });
 
 connection.connect(function(err) {
