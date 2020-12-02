14542a37adacb82516353afd43d36de0041c5850
cpusillo <cpusillo@gmail.com>
2020-12-02 13:27:48 -0500

    Cleardb mysql

--- db/schema.sql
+++ db/schema.sql
@@ -5,5 +5,6 @@ USE burgers_db;
 CREATE TABLE burgers (
     id integer auto_increment not null,
     burger_name varchar(100),
-    devoured boolean default false
+    devoured boolean default false,
+    primary key (id)
 )
